a37b1d19725d351e16c400febb669e172ddd6050
Puh Puhalev <gesho.puh@gmail.com>
2015-06-18 19:07:44 +0300

    results + starts lists

---
+++ startlists.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+  <?php 
+    include 'header.html';
+	$name = basename(__FILE__, '.php'); ;
+  ?>
+  <title>Start Lists/Results | European Athletics Team Championships 2nd League Stara Zagora 2015</title>
+</head>
+
+<body>
+    <?php 
+      include 'menu.html';
+    ?>
+	  <div id="content">
+        <div class="content_item">
+		  <h1>Start Lists / Results</h1> 
+						
+						<div class="content_item">
+                <div class="startListsResults">
+                          <h3>Start lists</h3>
+                          <p class="read_more" name="startLists">Read more</p>
+                          
+                          <div class="hidden" name="startLists">
+                            <p>Coming soon...</p>
+                          </div>
+                          <!--
+                          <p class="read_more" name="startListsMen">Men</p>
+                            <div class="hidden" name="startListsMen">
+                              <p><a href="results/men_start_list/100.xls">100m</a></p>
+                              <p><a href="results/men_start_list/100B.xls">100m B</a></p>
+                              <p><a href="results/men_start_list/200.xls">200m</a></p>
+                              <p><a href="results/men_start_list/400.xls">400m</a></p>
+                              <p><a href="results/men_start_list/800.xls">800m</a></p>
+                              <p><a href="results/men_start_list/1500.xls">1500m</a></p>
+                              <p><a href="results/men_start_list/3000.xls">3000m</a></p>
+                              <p><a href="results/men_start_list/5000.xls">5000m</a></p>
+                              <p><a href="results/men_start_list/110h.xls">110m Hurdles</a></p>
+                              <p><a href="results/men_start_list/400h.xls">400m Hurdles</a></p>
+                              <p><a href="results/men_start_list/3000sc.xls">3000m Steeplechase</a></p>
+                              <p><a href="results/men_start_list/4x400.xls">4x400m Relay</a></p>
+                              <p><a href="results/men_start_list/4x100.xls">4x100m Relay</a></p>
+                              <p><a href="results/men_start_list/high jump.xls">High Jump</a></p>
+                              <p><a href="results/men_start_list/pole vault.xls">Pole Vault</a></p>
+                              <p><a href="results/men_start_list/long jump.xls">Long Jump</a></p>
+                              <p><a href="results/men_start_list/triple jump.xls">Triple Jump</a></p>
+                              <p><a href="results/men_start_list/shot put.xls">Shot Put</a></p>
+                              <p><a href="results/men_start_list/discus throw.xls">Discus Throw</a></p>
+                              <p><a href="results/men_start_list/hammer throw.xls">Hammer Throw</a></p>
+                              <p><a href="results/men_start_list/javelin throw.xls">Javelin Throw</a></p>
+                            </div>
+
+                            <p class="read_more" name="startListsWomen">Women</p>
+                            <div class="hidden" name="startListsWomen">
+                              <p><a href="results/women_start_list/100.xls">100m</a></p>
+                              <p><a href="results/women_start_list/100B.xls">100m B</a></p>
+                              <p><a href="results/women_start_list/200.xls">200m</a></p>
+                              <p><a href="results/women_start_list/400.xls">400m</a></p>
+                              <p><a href="results/women_start_list/800.xls">800m</a></p>
+                              <p><a href="results/women_start_list/1500.xls">1500m</a></p>
+                              <p><a href="results/women_start_list/3000.xls">3000m</a></p>
+                              <p><a href="results/women_start_list/5000.xls">5000m</a></p>
+                              <p><a href="results/women_start_list/100h.xls">100m Hurdles</a></p>
+                              <p><a href="results/women_start_list/400h.xls">400m Hurdles</a></p>
+                              <p><a href="results/women_start_list/3000sc.xls">3000m Steeplechase</a></p>
+                              <p><a href="results/women_start_list/4x400.xls">4x400m Relay</a></p>
+                              <p><a href="results/women_start_list/4x100.xls">4x100m Relay</a></p>
+                              <p><a href="results/women_start_list/high jump.xls">High Jump</a></p>
+                              <p><a href="results/women_start_list/pole vault.xls">Pole Vault</a></p>
+                              <p><a href="results/women_start_list/long jump.xls">Long Jump</a></p>
+                              <p><a href="results/women_start_list/triple jump.xls">Triple Jump</a></p>
+                              <p><a href="results/women_start_list/shot put.xls">Shot Put</a></p>
+                              <p><a href="results/women_start_list/discus throw.xls">Discus Throw</a></p>
+                              <p><a href="results/women_start_list/hammer throw.xls">Hammer Throw</a></p>
+                              <p><a href="results/women_start_list/javelin throw.xls">Javelin Throw</a></p>
+                            </div>
+                          -->
+                        <hr class="teams"><br><br>
+						            <h3>Results</h3>
+                          <p class="read_more" name="results">Read more</p>
+                          
+                          <div class="hidden" name="results">
+                            <p>Coming soon...</p>
+                          </div>
+                          <!--
+                          <p class="read_more" name="startListsMen">Men</p>
+                            <div class="hidden" name="startListsMen">
+                              <p><a href="results/men_results/100.xls">100m</a></p>
+                              <p><a href="results/men_results/100B.xls">100m B</a></p>
+                              <p><a href="results/men_results/200.xls">200m</a></p>
+                              <p><a href="results/men_results/400.xls">400m</a></p>
+                              <p><a href="results/men_results/800.xls">800m</a></p>
+                              <p><a href="results/men_results/1500.xls">1500m</a></p>
+                              <p><a href="results/men_results/3000.xls">3000m</a></p>
+                              <p><a href="results/men_results/5000.xls">5000m</a></p>
+                              <p><a href="results/men_results/110h.xls">110m Hurdles</a></p>
+                              <p><a href="results/men_results/400h.xls">400m Hurdles</a></p>
+                              <p><a href="results/men_results/3000sc.xls">3000m Steeplechase</a></p>
+                              <p><a href="results/men_results/4x400.xls">4x400m Relay</a></p>
+                              <p><a href="results/men_results/4x100.xls">4x100m Relay</a></p>
+                              <p><a href="results/men_results/high jump.xls">High Jump</a></p>
+                              <p><a href="results/men_results/pole vault.xls">Pole Vault</a></p>
+                              <p><a href="results/men_results/long jump.xls">Long Jump</a></p>
+                              <p><a href="results/men_results/triple jump.xls">Triple Jump</a></p>
+                              <p><a href="results/men_results/shot put.xls">Shot Put</a></p>
+                              <p><a href="results/men_results/discus throw.xls">Discus Throw</a></p>
+                              <p><a href="results/men_results/hammer throw.xls">Hammer Throw</a></p>
+                              <p><a href="results/men_results/javelin throw.xls">Javelin Throw</a></p>
+                            </div>
+
+                            <p class="read_more" name="startListsWomen">Women</p>
+                            <div class="hidden" name="startListsWomen">
+                              <p><a href="results/women_results/100.xls">100m</a></p>
+                              <p><a href="results/women_results/100B.xls">100m B</a></p>
+                              <p><a href="results/women_results/200.xls">200m</a></p>
+                              <p><a href="results/women_results/400.xls">400m</a></p>
+                              <p><a href="results/women_results/800.xls">800m</a></p>
+                              <p><a href="results/women_results/1500.xls">1500m</a></p>
+                              <p><a href="results/women_results/3000.xls">3000m</a></p>
+                              <p><a href="results/women_results/5000.xls">5000m</a></p>
+                              <p><a href="results/women_results/100h.xls">100m Hurdles</a></p>
+                              <p><a href="results/women_results/400h.xls">400m Hurdles</a></p>
+                              <p><a href="results/women_results/3000sc.xls">3000m Steeplechase</a></p>
+                              <p><a href="results/women_results/4x400.xls">4x400m Relay</a></p>
+                              <p><a href="results/women_results/4x100.xls">4x100m Relay</a></p>
+                              <p><a href="results/women_results/high jump.xls">High Jump</a></p>
+                              <p><a href="results/women_results/pole vault.xls">Pole Vault</a></p>
+                              <p><a href="results/women_results/long jump.xls">Long Jump</a></p>
+                              <p><a href="results/women_results/triple jump.xls">Triple Jump</a></p>
+                              <p><a href="results/women_results/shot put.xls">Shot Put</a></p>
+                              <p><a href="results/women_results/discus throw.xls">Discus Throw</a></p>
+                              <p><a href="results/women_results/hammer throw.xls">Hammer Throw</a></p>
+                              <p><a href="results/women_results/javelin throw.xls">Javelin Throw</a></p>
+                            </div>
+                        </div>-->
+            </div>                        
+		</div><!--close content_item-->
+      </div><!--close content-->   
+	</div><!--close site_content-->  	
+  </div><!--close main-->
+  
+    <?php 
+      include 'footer.html';
+    ?>
+
+
+  <!-- javascript at the bottom for fast page loading -->
+  <script type="text/javascript" src="js/jquery.js"></script>
+  
+</body>
+</html>
---
+++ statistics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+  <?php 
+    include 'header.html';
+	$name = basename(__FILE__, '.php'); ;
+  ?>
+  <title>Statistics | European Athletics Team Championships 2nd League Stara Zagora 2015</title>
+</head>
+
+<body>
+    <?php 
+      include 'menu.html';
+    ?>
+	  <div id="content">
+        <div class="content_item">
+		  <h1>Statistics Handbook</h1>   
+          <p style="margin-top:20px;">
+          	<iframe src="pdf/Statistics Handbook ETC 2015 League 2.pdf" target="_blank"  width="95%" height="800px"><strong><a href="pdf/Statistics Handbook ETC 2015 League 2.pdf" target="_blank">View HERE</a></strong></iframe>
+          </p>
+		</div><!--close content_item-->
+      </div><!--close content-->   
+	</div><!--close site_content-->  	
+  </div><!--close main-->
+  
+    <?php 
+      include 'footer.html';
+    ?>
+
+
+  <!-- javascript at the bottom for fast page loading -->
+  <script type="text/javascript" src="js/jquery.js"></script>
+  
+</body>
+</html>
